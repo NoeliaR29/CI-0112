fa2dbaabcc3847f4749b820c518a74b6cc792751
NoeliaR29 <noeliaramirezrod@gmail.com>
2024-09-13 19:20:45 -0600

    carpeta y documentos

---
+++ Lab 2-simulacion robots/JuegoBatalla.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class JuegoBatalla {
+    private ArrayList<Robot> robots;
+    public JuegoBatalla() {
+        this.robots = new ArrayList();
+    }
+
+    public void iniciarJuego() {
+        Scanner ent = new Scanner(System.in);
+        int cantRobots;
+        do {
+            System.out.print("Ingrese la cantidad de robots, por favor entre 1 y 10: ");
+            cantRobots = ent.nextInt();
+            if (cantRobots < 1 || cantRobots > 10) {
+                System.out.println("Cantidad inválida, debe ser entre 1 y 10.");
+            }
+        } while (cantRobots < 1 || cantRobots > 10);
+        
+        for (int i = 0; i < cantRobots; i++) {
+            System.out.print("Ingrese el nombre del robot " + (i + 1) + ": ");
+            String nombre = ent.next();
+            System.out.print("Ingrese los puntos de vida para el robot: " + nombre + " ,debe ser entre 50 y 100");
+            int puntosVida = ent.nextInt();
+            while (puntosVida < 50 || puntosVida > 100) {
+                System.out.print("Valor ingresado inválido, por favor ingrese un número entre 50 y 100: ");
+                puntosVida = ent.nextInt();
+            }
+            System.out.print("Ingrese la fuerza de ataque para el robot:  " + nombre + " , debe ser entre 10 y 20");
+            int ataque = ent.nextInt();
+            while (ataque < 10 || ataque > 20) {
+                System.out.print("Valor inválido. Ingrese un número entre 10 y 20: ");
+                ataque = ent.nextInt();
+            }
+            System.out.print("Ingrese la defensa (entre 0 y 10) para " + nombre + ": ");
+            int defensa = ent.nextInt();
+            while (defensa < 0 || defensa > 10) {
+                System.out.print("Valor inválido. Ingrese un número entre 0 y 10: ");
+                defensa = ent.nextInt();
+            }
+            robots.add(new Robot(nombre, puntosVida, ataque, defensa));
+        }
+        iniciarBatalla();
+    }
+
+    public void iniciarBatalla() {
+    while (robots.size() > 1) {
+        for (int i = 0; i < robots.size(); i++) {
+            Robot atacante = robots.get(i);
+            if (atacante.getPuntosVida() <= 0) {
+                robots.remove(i);
+                i--; 
+                continue;
+            }
+            int indDefensor = (int) (Math.random() * robots.size());
+            Robot defensor = robots.get(indDefensor);
+            if (atacante != defensor) {
+                System.out.println(atacante.getNombre() + " ataca a " + defensor.getNombre());
+                atacante.atacar(defensor);
+                if (defensor.getPuntosVida() <= 0) {
+                    System.out.println(defensor.getNombre() + " ha sido destruido.");
+                    robots.remove(defensor);
+                }
+            }
+        }
+    }
+    Ganador();
+    }
+
+    public void Ganador() {
+        if (robots.size() == 1) {
+         Robot ganador = robots.get(0);
+            System.out.println("El ganador del juego es: " + ganador.getNombre());
+        } else {
+            System.out.println("no hay vivos.");
+        }
+    }
+
+    public static void main(String[] args) {
+        JuegoBatalla juego = new JuegoBatalla();
+        juego.iniciarJuego();
+    }
+}
---
+++ Lab 2-simulacion robots/Robot.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+public class Robot {
+    private String nombre; private int puntosVida; private int ataque; private int defensa;
+    public Robot(String nombre, int puntosVida, int ataque, int defensa) {
+        this.nombre = nombre;
+        this.puntosVida = puntosVida;
+        this.ataque = ataque;
+        this.defensa = defensa;
+    }
+
+    public void atacar(Robot otroRobot) {
+        int daño = this.ataque - otroRobot.getDefensa(); 
+        if (daño > 0) {
+            otroRobot.reducirVida(daño);
+            System.out.println(this.nombre + " le bajó " + daño + " puntos de vida a: "+ otroRobot.nombre +".");
+            System.out.println(otroRobot.getNombre() + " ahora tiene " + otroRobot.getPuntosVida() + " puntos de vida.");
+            System.out.println("------------------");
+        } else {
+            System.out.println(this.nombre + " no pudo dañar a " + otroRobot.getNombre());
+        }
+    }
+    
+    public void reducirVida(int daño) {
+        this.puntosVida -= daño;
+        if (this.puntosVida < 0) {
+            this.puntosVida = 0;
+        }
+    }
+
+    public boolean estaVivo() {
+        return this.puntosVida > 0;
+    }
+
+    public int getPuntosVida() {
+        return puntosVida;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public String toString() {
+        return "Robot: " + nombre + " , Vida: " + puntosVida + " , Ataque: " + ataque + " , Defensa: " + defensa;
+    }
+}
