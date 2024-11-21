a2faf204afb3872dc1147c771ead067a0f02e0ff
Adrian <ADRIAN.QUIROSGARITA@@UCR.AC.CR>
2024-11-20 21:00:36 -0600

    Nodo para la lista doblemente enlazada

---
+++ proyecto_2/ListaDoblementeEnlazada.java
@@ -0,0 +1,13 @@
+public class ListaDoblementeEnlazada {
+    private Nodo cabeza;
+    private Nodo cola;
+
+    public ListaDoblementeEnlazada(){
+        this.cabeza = null;
+        this.cola = null;
+    }
+
+    public void agregarNodo(int dato){
+        
+    }
+}
